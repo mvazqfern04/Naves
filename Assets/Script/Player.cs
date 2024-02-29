2fab9b5e7e61a090ac35553bb26476643af2013e
mario <mario.vazquez.fernandez@gmail.com>
2024-02-29 22:01:11 +0100

    subo mucho

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,62 +1,124 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor.U2D;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private Rigidbody2D rb;
-    [SerializeField] private float maxima_velocidad = 30f;
-    [SerializeField] private float speed = 0.9f;
-    private float frenada;
-    [SerializeField] private int rotation = 40;
+    private float speed = 0f;
+    [SerializeField] private float velMax;
+    [SerializeField] private float rotacion;
+
+    private bool isAlive = true;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        frenada = speed*2f;
-        rb= GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        if (isAlive)
         {
-            acelerar();
+            move();
         }
-        if (Input.GetKey(KeyCode.Space))
-        {
-            frenar();
-        }
-        girar(Input.GetAxisRaw("Horizontal")) ;
     }
 
-    private void acelerar()
+    private void move()
     {
-        //rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y+speed*Time.deltaTime);
-        rb.velocity = Vector2.up*speed;
-    }
-    private void frenar()
-    {
-        if( rb.velocity.y>0f)
+        float rotacionDirect = Input.GetAxisRaw("Horizontal");
+        float movement = Input.GetAxisRaw("Vertical");
+
+        if (rotacionDirect<0)
         {
-            //rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y-frenada*Time.deltaTime);
-            rb.velocity = Vector2.down*speed;
+            transform.Rotate(0f, 0f, rotacion*Time.deltaTime, Space.Self);
+        }
+        else if (rotacionDirect>0)
+        {
+            transform.Rotate(0f, 0f, -rotacion*Time.deltaTime, Space.Self);
         }
         else
-        {       
-            rb.velocity =Vector2.zero;
+        {
+            //Idle
         }
+        //Chad
+        if (movement == 0f)
+        {
+            //No toca nada
+            if (speed > 0f)
+            {
+                speed-=0.05f;
+            }
+        }
+        //Para Alante
+        else if (movement >= 1f)
+        {
+            //Acelera
+            if (speed < velMax)
+            {
+                speed += 0.5f;
+            }
+            //TOPE
+            else if (speed >= velMax)
+            {
+                speed=velMax;
+            }
+        }
+        //Para Atras
+        else if (movement <=-1f)
+        {
+            //Quieto
+            if (speed <=0f)
+            {
+                speed=0f;
+            }
+            //Freno
+            else if (speed >0)
+            {
+                speed -= 0.75f;
+            }
+        }
+        transform.Translate(Vector3.up*speed*Time.deltaTime);
+
+        //Debug
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.position = new Vector3(0, 0, 0);
+        }
+
     }
 
-    private void girar(float direccion)
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        if (direccion==0)
+        if (collision.tag == "limite")
         {
-            return;
+            if (collision.transform.position.x == 0)
+            {
+                if (collision.transform.position.y >0)
+                {
+                    transform.position = new Vector3(transform.position.x, transform.position.y*-1+0.1f, 0);
+                }
+                else
+                {
+                    transform.position = new Vector3(transform.position.x, transform.position.y*-1-0.1f, 0);
+                }
+            }
+            else
+            {
+                if (collision.transform.position.x >0)
+                {
+                    transform.position = new Vector3(transform.position.x*-1+0.1f, transform.position.y, 0);
+                }
+                else
+                {
+                    transform.position = new Vector3(transform.position.x*-1-0.1f, transform.position.y, 0);
+                }
+            }
         }
-        gameObject.transform.Rotate(0,0,direccion*rotation);
     }
 }
